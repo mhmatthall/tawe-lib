369cc272c51530b8153c5e30c560ecaef5bf13a6
Constantinos Loizou <45148735+tefkros777@users.noreply.github.com>
2018-11-25 20:50:11 +0000

    Create Copy.java

---
+++ Copy.java
@@ -0,0 +1,14 @@
+/*
+ * @author
+ * 		Constantinos Loizou
+ */
+public class Copy {
+	
+	private static String nextCopyID;
+	
+	private String copyID;
+	private String resourceID;
+	private int LoanTime;
+	
+	
+}
