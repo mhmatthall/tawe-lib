032ffe0f384d8e87f7f4b70b04b6c1e40b00fb28
Matt <42594962+mhmatthall@users.noreply.github.com>
2018-11-27 16:08:39 +0000

    Added inital DB connection

--- DatabaseRequest.java
+++ DatabaseRequest.java
@@ -1,3 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /* DatabaseRequest
    @author Matt Hall
 */
+
+public class DatabaseRequest {
+	private static final String databaseName = "tawe-lib";
+	
+	public static void main(String[] args) {
+		establishConnection();
+	}
+	
+	public static void establishConnection() {
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection("jdbc:derby:" + databaseName);
+			System.out.println("Connection successful?: " + conn.isValid(0));
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
