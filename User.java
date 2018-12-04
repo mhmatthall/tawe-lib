82ae5a5187a09daa568a64ccc775d914bfde63b2
Constantinos Loizou <45148735+tefkros777@users.noreply.github.com>
2018-12-04 15:57:33 +0000

    Added isLibrarian attribute and method

--- User.java
+++ User.java
@@ -2,28 +2,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class User {
 	private String username;
 	private String forename;
 	private String surname;
 	private String phoneNumber;
-	private String address;	
+	private String address;
 	private UserImage profileImage;
-	
-	public User(String username, String forename, String surname, String phoneNumber, String address, UserImage profileImage){
+	protected boolean isLibrarian;
+
+	public User(String username, String forename, String surname, String phoneNumber, String address,
+		UserImage profileImage) {
 		this.username = username;
 		this.forename = forename;
 		this.surname = surname;
 		this.phoneNumber = phoneNumber;
 		this.address = address;
 		this.profileImage = profileImage;
+		
 	}
-	
+
 	public String getSurname() {
 		return surname;
 	}
-	
+
 	public String getForename() {
 		return forename;
 	}
@@ -31,19 +33,19 @@ public class User {
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public String getPhoneNumber() {
 		return phoneNumber;
-	}	
-	
+	}
+
 	public String getAddress() {
 		return address;
 	}
-	
+
 	public UserImage getProfileImage() {
 		return profileImage;
 	}
-	
+
 //	public ArrayList[] browseResources(){
 //		ArrayList[] DVDs = new ArrayList[DatabaseRequest.viewTable("DVD")];
 //		ArrayList[] books = new ArrayList[DatabaseRequest.viewTable("Book")];
@@ -64,10 +66,10 @@ public class User {
 //	
 //	}
 //	
-//	public boolean isLibrarian() {
-//		return isLibrarian;
-//	}
+	public boolean isLibrarian() {
+		return isLibrarian;
+	}
 //	
 //	public loadUserData(){
 //	
-	}
+}
