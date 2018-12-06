2b6a706b6bbd1e542506cb94b5a4287e60f6ddee
Matt <42594962+mhmatthall@users.noreply.github.com>
2018-12-06 13:09:09 +0000

    Added missing constructors, overridden constructor
    New constructor for database loading

--- Copy.java
+++ Copy.java
@@ -5,16 +5,55 @@
  * TODO fix constructor to add missing variables
  */
 public class Copy {
-	
 	private static int nextCopyID;
 	private String copyID = ("C" + nextCopyID);
 	private String resourceID;
 	private int loanTime;
+	private boolean isOnLoan;
+	private boolean isReserved;
+	private String reservingUser;	// Username of user reserving the copy
 	
 	public Copy(String resourceID, int loanTime) {
 		this.resourceID = resourceID;
 		this.loanTime = loanTime;
+		isOnLoan = false;
+		isReserved = false;
+		reservingUser = "";
 		nextCopyID++;
+	}
+	
+	public Copy(String copyID, String resourceID, int loanTime, boolean isOnLoan, boolean isReserved, String reservingUser) {
+		// This constructor required for loading copies from the database
+		this.copyID = copyID;
+		this.resourceID = resourceID;
+		this.loanTime = loanTime;
+		this.isOnLoan = isOnLoan;
+		this.isReserved = isReserved;
+		this.reservingUser = reservingUser;
+	}
+	
+	public boolean isOnLoan() {
+		return isOnLoan;
+	}
+
+	public void setOnLoan(boolean isOnLoan) {
+		this.isOnLoan = isOnLoan;
+	}
+
+	public boolean isReserved() {
+		return isReserved;
+	}
+
+	public void setReserved(boolean isReserved) {
+		this.isReserved = isReserved;
+	}
+
+	public String getReservingUser() {
+		return reservingUser;
+	}
+
+	public void setReservingUser(String reservingUser) {
+		this.reservingUser = reservingUser;
 	}
 
 	public String getCopyID() {
@@ -39,7 +78,5 @@ public class Copy {
 
 	public void setLoanTime(int loanTime) {
 		this.loanTime = loanTime;
-	}
-	
-	
+	}	
 }
