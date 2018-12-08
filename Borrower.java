a7a23b1e2f7572ffb6aa11f79b5bad455dd23d26
Rimantas Kazlauskas <konulv731@gmail.com>
2018-12-08 21:59:12 +0000

    forgot ;

--- Borrower.java
+++ Borrower.java
@@ -39,7 +39,7 @@ public class Borrower extends User {
 	 */
 	public ArrayList<Copy> getLoanedCopies() throws SQLException {
 		DatabaseRequest db = new DatabaseRequest();
-		return null //db.getUserLoans(getUsername());
+		return null; //db.getUserLoans(getUsername());
 	}
 	
 	/**
