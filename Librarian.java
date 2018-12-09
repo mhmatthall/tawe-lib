4434605de17116d9b88e211ac50e54c05922b873
Rimantas Kazlauskas <konulv731@gmail.com>
2018-12-09 23:21:15 +0000

    javadoc

--- Librarian.java
+++ Librarian.java
@@ -1,37 +1,76 @@
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-/*
- * Created by Matt
+/**
+ * @author Matt Hall
  */
 public class Librarian extends User {
 	protected int staffNumber;
 	protected Date employmentDate;
 
-	public Librarian(String username, String forename, String surname, String phoneNumber, String address,
-			UserImage profileImage, int staffNumber, Date employmentDate) {
+	/**
+	 * Used for creating a new librarian
+	 *
+	 * @param username of the librarian
+	 * @param forename of the librarian
+	 * @param surname of the librarian
+	 * @param phoneNumber of the librarian
+	 * @param address of the librarian
+	 * @param profileImage avatar of the librarian
+	 * @param staffNumber of the librarian
+	 * @param employmentDate of the librarian
+	 */
+	public Librarian(String username, String forename, String surname, String phoneNumber, 
+			String address,	UserImage profileImage, int staffNumber, Date employmentDate) {
 		super(username, forename, surname, phoneNumber, address, profileImage);
 		this.staffNumber = staffNumber;
 		this.employmentDate = employmentDate;
 		this.isLibrarian = true;
 	}
 
+	/**
+	 * Gets the staff number.
+	 *
+	 * @return the staff number
+	 */
 	public int getStaffNumber() {
 		return staffNumber;
 	}
 
+	/**
+	 * Sets the staff number.
+	 *
+	 * @param staffNumber to be set
+	 */
 	public void setStaffNumber(int staffNumber) {
 		this.staffNumber = staffNumber;
 	}
 
+	/**
+	 * Gets the employment date.
+	 *
+	 * @return the employment date
+	 */
 	public Date getEmploymentDate() {
 		return employmentDate;
 	}
 
+	/**
+	 * Sets the employment date.
+	 *
+	 * @param employmentDate to be changed to
+	 */
 	public void setEmploymentDate(Date employmentDate) {
 		this.employmentDate = employmentDate;
 	}
 
+	/**
+	 * Issues any available copy to a given user.
+	 *
+	 * @param resourceID to look up the copy for
+	 * @param username of a borrower
+	 * @throws SQLException if connection to the database has failed
+	 */
 	public void issueLoan(String resourceID, String username) throws SQLException {
 
 		DatabaseRequest db = new DatabaseRequest();
@@ -43,6 +82,12 @@ public class Librarian extends User {
 
 	}
 
+	/**
+	 * Return loan.
+	 *
+	 * @param loan to be returned
+	 * @throws SQLException if connection to database has failed
+	 */
 	public void returnLoan(Loan loan) throws SQLException {
 
 		DatabaseRequest db = new DatabaseRequest();
@@ -67,13 +112,20 @@ public class Librarian extends User {
 		db.editLoan(loan);
 	}
 
+	/**
+	 * Pay fine.
+	 *
+	 * @param fineID of a fine to be paid
+	 * @param amount to be paid
+	 * @throws SQLException if connection to database has failed
+	 */
 	public void payFine(String fineID, double amount) throws SQLException {
 
 		DatabaseRequest db = new DatabaseRequest();
 		Fine f = db.getFine(fineID);
 
 		if (amount < f.getMinimumPayment())
-			throw new IllegalArgumentException("Cannot pay less than £" + f.getMinimumPayment());
+			throw new IllegalArgumentException("Cannot pay less than ï¿½" + f.getMinimumPayment());
 		else if (f.getAmountPaid() + amount > f.getAmount()) {
 			throw new IllegalArgumentException("Cant pay more than the total fine amount");
 		}
@@ -84,6 +136,13 @@ public class Librarian extends User {
 
 	}
 
+	/**
+	 * Requests a resource.
+	 *
+	 * @param resourceID to be requested
+	 * @param username of a user that is requesting
+	 * @throws SQLException if the connection to database has failed
+	 */
 	public void requestResource(String resourceID, String username) throws SQLException {
 
 		DatabaseRequest db = new DatabaseRequest();
@@ -101,6 +160,13 @@ public class Librarian extends User {
 		}
 	}
 
+	/**
+	 * Reserve resource.
+	 *
+	 * @param resourceID of a resource to be reserved
+	 * @param username of a user who is reserving
+	 * @throws SQLException if connection to the database has failed
+	 */
 	public void reserveResource(String resourceID, String username) throws SQLException {
 
 		DatabaseRequest db = new DatabaseRequest();
